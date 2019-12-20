8d397d6d76aaf8c681bd91e0d3814fc189a709df
Travis Hathaway <travis.j.hathaway@gmail.com>
2019-12-20 13:09:22 +0100

    Adding command to show work log messages

--- trkr/commands/show.py
+++ trkr/commands/show.py
@@ -1,22 +1,36 @@
+import sys
+
 import click
 from tabulate import tabulate
 
 from trkr.data.connect import get_connection
+from trkr.data.read import get_all_projects, get_all_logs
 
-LIST_FIELDS = ('id', 'name')
+LIST_FIELDS_PROJECTS = ('id', 'name', 'total_hours')
+LIST_FIELDS_LOGS = ('id', 'name', 'description', 'hours')
 
-LIST_PROJECTS = f"""
-SELECT {','.join(LIST_FIELDS)} FROM projects
-"""
+TYPE_PROJECT = 'project'
+TYPE_LOG = 'log'
 
 
 @click.command()
-def show():
+@click.option('-d', '--data-type', default=TYPE_PROJECT,
+              help='Project name (default used if none provided)')
+def show(data_type):
     """
     Shows the available projects
     """
     conn = get_connection()
     cur = conn.cursor()
-    res = cur.execute(LIST_PROJECTS)
 
-    click.echo(tabulate(res.fetchall(), headers=LIST_FIELDS))
+    if data_type == TYPE_PROJECT:
+        data = get_all_projects(cur)
+        headers = LIST_FIELDS_PROJECTS
+    elif data_type == TYPE_LOG:
+        data = get_all_logs(cur)
+        headers = LIST_FIELDS_LOGS
+    else:
+        click.echo(f"Invalid type, available choices are '{TYPE_PROJECT}' and '{TYPE_LOG}'")
+        sys.exit(1)
+
+    click.echo(tabulate(data, headers=headers))
--- trkr/data/read.py
+++ trkr/data/read.py
@@ -1,6 +1,40 @@
 
 GET_PROJECT_BY_NAME_SQL = """
-    SELECT id, name FROM projects WHERE name = ?
+    SELECT 
+        id, name
+    FROM 
+        projects 
+    WHERE
+        name = ?
+"""
+
+GET_ALL_PROJECTS_SQL = """
+    SELECT 
+        projects.id, 
+        projects.name, 
+        sum(work_log.hours)
+    FROM 
+        projects 
+    LEFT JOIN
+        work_log
+    ON
+        work_log.project_id = projects.id
+    GROUP BY
+        projects.id, projects.name
+"""
+
+GET_ALL_LOGS_SQL = """
+    SELECT 
+        projects.id, 
+        projects.name,
+        work_log.description,
+        work_log.hours
+    FROM 
+        work_log 
+    LEFT JOIN
+        projects
+    ON
+        work_log.project_id = projects.id
 """
 
 
@@ -8,9 +42,31 @@ def get_project_by_name(cursor, *args) -> tuple:
     """
     Gets a project from the database using the name.
 
-    :param cursor: database cursor
+    :param cursor: database cursor (SQLite)
     :param args: List of args to pass to insert statement
     """
     res = cursor.execute(GET_PROJECT_BY_NAME_SQL, args)
 
     return res.fetchone()
+
+
+def get_all_projects(cursor) -> tuple:
+    """
+    Returns all projects
+
+    :param cursor: database cursor (SQLite)
+    """
+    res = cursor.execute(GET_ALL_PROJECTS_SQL)
+
+    return res.fetchall()
+
+
+def get_all_logs(cursor) -> tuple:
+    """
+    Returns all work logs
+
+    :param cursor: database cursor (SQLite)
+    """
+    res = cursor.execute(GET_ALL_LOGS_SQL)
+
+    return res.fetchall()
